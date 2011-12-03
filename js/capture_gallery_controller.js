433d44cc10bf3382915870c3af08d9ea41460fa2
James Williams <james.l.williams@gmail.com>
2011-12-03 13:23:03 -0800

    Saving and updating gallery works
    Saving works.
    Updates to gallery work.

--- js/capture_gallery_controller.js
+++ js/capture_gallery_controller.js
@@ -2,6 +2,8 @@
  * Capture Gallery controller to display the preview
  * for the hangout.
  *
+ * @authors Mohamed Mansour 2011 (http://mohamedmansour.com)
+ * @author James Williams 2011 (http://jameswilliams.be)
  * @constructor
  */
 CaptureGalleryController = function() {
@@ -29,6 +31,7 @@ CaptureGalleryController.prototype.bindUIControls = function() {
   $('.download').click(this.downloadCapture.bind(this));
   $('.effects').click(this.showEffectsWindow.bind(this));
   $('.discard').click(this.hideEffectsWindow.bind(this));
+  $('.save').click(this.onSaveClicked.bind(this));
 };
 
 CaptureGalleryController.prototype.renderGallery = function() {
@@ -80,6 +83,9 @@ CaptureGalleryController.prototype.downloadCapture = function(e) {
 
 CaptureGalleryController.prototype.showEffectsWindow = function(e) {
   var container = $(e.target).parent().parent().parent();
+  // save thumbnail data for later
+  controller.currentThumbnail = container.find('img').get(0);
+  // retrieve larger image
   chrome.extension.sendRequest({
     service: 'Capture',
     method: 'findCapture',
@@ -109,6 +115,64 @@ CaptureGalleryController.prototype.replaceEffectsCanvas = function() {
 }
 
 CaptureGalleryController.prototype.hideEffectsWindow = function() {
+  console.log($(controller.glfxCanvas));
+  $(controller.glfxCanvas).remove();
   $('#light').css('display','none');
   $('#fade').css('display','none');
+};
+
+CaptureGalleryController.prototype.onSaveClicked = function() {  
+  var originalData = {};
+  originalData.active = controller.glfxCanvas.toDataURL();
+    originalData.active_width = controller.glfxCanvas.width;
+    originalData.active_height = controller.glfxCanvas.height;
+    originalData.time = new Date();
+    originalData.description = "Applied Filter to existing image.";
+    
+  // maybe refactor to use send request
+  //previewController = new CapturePreviewController();
+  var thumbnailDimensions = {width: controller.currentThumbnail.width, 
+    height:controller.currentThumbnail.height};
+    
+  processCapture = function(imgData) {
+    console.log("process capture");
+    console.log(imgData);
+    originalData.thumbnail = imgData;
+    originalData.thumbnail_width = thumbnailDimensions.width;
+    originalData.thumbnail_height = thumbnailDimensions.height;
+    
+    
+    chrome.extension.sendRequest({
+      service: 'Capture',
+      method: 'processCapture',
+      arguments: [originalData]
+    }, function(res) {
+      console.log(res);
+      console.log("saved");
+      $('#light').hide();
+      $('#fade').hide();
+      $(controller.glfxCanvas).remove();
+      controller.refreshGallery();
+    });
+    
+  }
+  
+  // glfx.js doesn't seem to work nicely with drawImage so I had to write to an image
+  // and then use that image with drawImage
+  tempImage = new Image()
+  tempImage.src = controller.glfxCanvas.toDataURL();
+  tempImage.onload = function () {
+      tempCanvas = document.createElement("canvas");
+      ctx = tempCanvas.getContext("2d");
+      ctx.canvas.width = thumbnailDimensions.width;
+      ctx.canvas.height = thumbnailDimensions.height;
+      ctx.drawImage(this, 0, 0, thumbnailDimensions.width, thumbnailDimensions.height);
+    
+      processCapture(ctx.canvas.toDataURL());    
+    }                                    
+}
+
+CaptureGalleryController.prototype.refreshGallery = function() {
+  $('.gallery').empty();
+  this.renderGallery();
 };
