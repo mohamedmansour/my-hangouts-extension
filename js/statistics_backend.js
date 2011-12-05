ac4d13f5fa378407df7f1ca7524e0ba5f3690821
Mohamed Mansour <hello@mohamedmansour.com>
2011-12-04 23:56:14 -0500

    Implement Statistics Backend for Extension.
    For now, we just design the model, the implementation not ready
    yet, will soon. Gathering statics for each hangout so we can
    improve the rankings for each hangout.

---
+++ js/statistics_backend.js
@@ -0,0 +1,31 @@
+/**
+ * Database backend for hangout statistics.
+ *
+ * @constructor
+ */
+StatisticsEntity =  = function(db) {
+  AbstractEntity.call(this, db, 'statistics');
+};
+JSAPIHelper.inherits(StatisticsEntity, AbstractEntity);
+
+/**
+ * @see AbstractEntity.tableDefinition
+ */
+StatisticsEntity.prototype.tableDefinition = function() {
+  return {
+    hangout_id: 'TEXT',
+    start_time: 'DATE',
+    end_time: 'DATE',
+    participants: 'TEXT' // Comma separated IDs
+  };
+};
+
+
+/**
+ * Backend that is responsible of handling hangout statistics.
+ * @param {Database} db The database backend.
+ * @constructor
+ */
+StatisticsBackend = function(db) {
+  this.statisticsEntity = new StatisticsEntity(db);
+};
