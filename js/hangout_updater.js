b3ec6b7129994ebe38bae44826e2bf3ad30ab81f
John Barrington Craggs <john.bc@gmail.com>
2011-12-17 22:01:19 -0800

    =fix notifier bug

--- js/hangout_updater.js
+++ js/hangout_updater.js
@@ -265,9 +265,13 @@ HangoutUpdater.prototype.update = function(refreshDeprecated) {
   }
   
   this.updatingResult = false;
-  
-  this.circleNotifier.notify(this.hangouts);
-  this.controller.drawBadgeIcon(this.hangouts.length, true);
+  this.updateDependants();
+}
+
+HangoutUpdater.prototype.updateDependants = function() {
+  var hangouts = this.getHangouts();
+  this.circleNotifier.notify(hangouts);
+  this.controller.drawBadgeIcon(hangouts.length, true);
 }
 
 HangoutUpdater.prototype.cleanHangouts = function() {
@@ -276,6 +280,7 @@ HangoutUpdater.prototype.cleanHangouts = function() {
   }
   
   this.cleaningHangouts = true;
+  
   var j = 0;
   for( var i = 0; i < this.hangouts.length; i++) {
     if ( !this.hangouts[j] ) {
@@ -289,7 +294,7 @@ HangoutUpdater.prototype.cleanHangouts = function() {
   for (var i = 0; i < this.hangouts.length; i++) {
     var hangout = this.hangouts[i];
     var id = hangout.data.id
-    console.log('checking for dead hangout: '+id);
+   // console.log('checking for dead hangout: '+id);
     var url = hangout.url;
     var postId = url.substring(url.lastIndexOf('/')+1);
     this.controller.plus.lookupPost(function(res) {
@@ -301,9 +306,8 @@ HangoutUpdater.prototype.cleanHangouts = function() {
 
   
   this.cleaningHangouts = false;
+  this.updateDependants();
 
-  this.circleNotifier.notify(this.hangouts);
-  this.controller.drawBadgeIcon(this.hangouts.length, true);
 };
 
 HangoutUpdater.prototype.removeHangout = function(id){
