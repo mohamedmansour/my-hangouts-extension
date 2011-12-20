9216a7041a3fbe7fafa3689d441f8112b5dc9e63
Mohamed Mansour <hello@mohamedmansour.com>
2011-12-20 16:51:13 -0500

    Removed blocked circle from My Hangouts
    Later on, we would need to support this, since it is valuable
    data.

--- js/background_controller.js
+++ js/background_controller.js
@@ -7,6 +7,7 @@
 BackgroundController = function() {
   var db = this.initDatabase();
   
+  this.BLOCKED_CIRCLE_ID = '15';
   this.UPDATE_INTERVAL = 30000; // Every 30 seconds.
   this.UPDATE_CIRCLES_INTERVAL = 1000 * 60 * 60 + 15000; // Every hour and 15 seconds;
   this.REFRESH_INTERVAL = 2000; // Look for new results every 5 seconds.
@@ -216,6 +217,18 @@ BackgroundController.prototype.refreshCircles = function() {
     self.plus.refreshCircles(function(status) {
       self.plus.getCircles(function(res) {
         if (res.status) {
+          // Go through the circles, and remove the blocked circle.
+          // TODO: Support showing blocked circles.
+          var bannedCircle = null;
+          res.data.some(function(circle, index) {
+            if (circle.id == self.BLOCKED_CIRCLE_ID) {
+              bannedCircle = index;
+              return true;
+            }
+          });
+          if (bannedCircle) {
+            res.data = res.data.slice(bannedCircle);
+          }
           self.myCircles = res.data;
         }
       });
