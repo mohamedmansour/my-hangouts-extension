aaa154ad543a0bb8a0481b66006583854355e3f5
Mohamed Mansour <hello@mohamedmansour.com>
2011-12-20 02:05:05 -0500

    Linkify each clickable notification link
    Images are now linkable and the join button now works. Changed
    the position to have join at the right. More UI changes is
    needed.

--- js/modules/circle_notifier/notification_controller.js
+++ js/modules/circle_notifier/notification_controller.js
@@ -3,6 +3,21 @@ NotificationController = function() {
 };
 
 NotificationController.prototype.init = function() {
+  $(document).on('click', 'a.clickable', this.onLinkClicked);
+};
+
+/**
+ * Forward click events to the extension.
+ *
+ * @param{MouseEvent} e The link which was clicked.
+ */
+NotificationController.prototype.onLinkClicked = function(e) {
+  e.preventDefault();
+  var href = $(e.target).attr('href');
+  if (!href) {
+    href = $(e.target).parent().attr('href');
+  }
+  chrome.tabs.create({url: href});
 };
 
 NotificationController.prototype.refresh = function(hangouts) {
